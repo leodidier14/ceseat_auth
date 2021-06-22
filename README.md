d41b5fbf5320f7589275966d4bd17f6339c9ddc5
Guiguiprog <33095251+Guiguiprog@users.noreply.github.com>
2021-06-22 14:43:24 +0200

    Add Auth API

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ceseat_auth
+Auth_API :
+POST /login -> vérifie les identifiants du user et donne un token si OK
+POST /accesstoken -> vérifie si le token est en cours d'utilisation renvoie true ou false
+PUT /logout -> Met fin à la connexion (kill le token existant) 
---
+++ routes/routes.js
@@ -0,0 +1,31 @@
+//Load required elements
+const router = require('express').Router()
+// const bcrypt = require('bcryptjs')
+const path = require('path')
+const express = require('express')
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
+// const jwt = require('jsonwebtoken');
+
+//Load user model
+// const User = require('../model/user')
+
+//Load validation functions
+const {logincontroller} = require('../controller/routeController')
+const {logoutcontroller} = require('../controller/routeController')
+const {accesstokencontroller} = require('../controller/routeController')
+
+//Use json parser
+router.use(express.json());
+
+//Routes
+router.post('/login', function(req, res){
+    logincontroller(req, res)
+});
+router.put('/logout', function(req, res){
+    logoutcontroller(req, res)
+});
+router.post('/accesstoken', function(req, res){
+    accesstokencontroller(req, res)
+});
+
+module.exports = router;
